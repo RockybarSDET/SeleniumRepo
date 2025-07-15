c358c379b34c4815e2935ebc37ce03c26e7848ae
Acer <Acer@192.168.1.36>
2025-07-16 05:17:34 +0530

    First commit for selenium webdriver

---
+++ src/test/java/selenium/LaunchBrowser.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+
+@Test
+    public void openGoogleInChrome() {
+        // Set the path to chromedriver (optional if chromedriver is in system PATH)
+        System.setProperty("webdriver.chrome.driver", "C:\\DriversChrome\\chromedriver.exe");
+
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.flipkart.com");
+
+        // Optional: Maximize the browser window
+        driver.manage().window().maximize();
+
+        // Optional: Print page title
+        System.out.println("Page Title is: " + driver.getTitle());
+
+        // Optional: Close the browser after few seconds
+        try {
+            Thread.sleep(3000); // wait for 5 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(driver.getTitle(), "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!");
+
+        driver.quit();
+    }
+}
